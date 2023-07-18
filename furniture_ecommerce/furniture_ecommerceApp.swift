1f239c65ee19f23aac0a76433a4ba8fb31eea806
daveotengo <30934250+daveotengo@users.noreply.github.com>
2023-07-18 22:18:44 +0000

    Initial Commit

---
+++ furniture_ecommerce/furniture_ecommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  furniture_ecommerceApp.swift
+//  furniture_ecommerce
+//
+//  Created by David Oteng on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct furniture_ecommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
